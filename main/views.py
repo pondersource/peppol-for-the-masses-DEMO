d425e859de475a717458f2cfb8dfd4d7dfba8599
Triantafullenia-Doumani <triantafulleniad@gmail.com>
2021-05-26 17:55:38 +0300

    Add all my files

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+
+# Create your views here.
+
+def index(response,id):
+    ls = ToDoList.objects.get(id = id)
+    return render(response , "main/base.html",{"name":ls.name})
+
+def home(response):
+    return render(response , "main/home.html",{"name":"test"})
