74b3083239230a006a5731b0901dc45f07801724
Triantafullenia-Doumani <triantafulleniad@gmail.com>
2021-06-09 16:23:33 +0300

    Fix issue, close #7

--- invoice/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Invoice(models.Model):
-
-    website = models.URLField(max_length = 200)
-    first_name = models.CharField(max_length=140)
-    last_name = models.CharField(max_length=50)
--- invoice/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("invoiceSend/",views.invoice , name="invoiceSend"),
-    path("invoice/",views.invoice,name="invoice"),
-]
