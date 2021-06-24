4887952f1e8fd40d798d29f9cabe2d71ad163843
Triantafullenia-Doumani <Triantafulleniad@gmail.com>
2021-06-24 17:52:14 +0300

    interface updates

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,14 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from main.views import IndexPageView, ChangeLanguageView
+from main.views import IndexPageView, ChangeLanguageView , MessagesView , ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', IndexPageView.as_view(), name='index'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('messages/', MessagesView.as_view(), name='messages'),
 
     path('i18n/', include('django.conf.urls.i18n')),
     path('language/', ChangeLanguageView.as_view(), name='change_language'),
