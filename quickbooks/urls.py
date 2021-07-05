a28688067c0cc9205a79c648d41dff85ec575b4e
Michiel de Jong <michiel@unhosted.org>
2021-07-05 13:36:41 +0200

    Copy from https://github.com/IntuitDeveloper/SampleOAuth2_UsingPythonClient

--- quickbooks/urls.py
+++ quickbooks/urls.py
@@ -1,10 +1,20 @@
-from django.conf.urls import re_path
-from django.views.generic import RedirectView
+from django.conf.urls import url
+from django.views.generic.base import RedirectView
+from . import views
 
-from quickbooks.views import *
+app_name = 'app'
 
-app_name = 'quickbooks'
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
-    re_path(r'^$', connect, name='quickbooks_connect'),
+    url(r'^$', views.index, name='index'),
+    url(r'^oauth/?$', views.oauth, name='oauth'),
+    url(r'^openid/?$', views.openid, name='openid'),
+    url(r'^callback/?$', views.callback, name='callback'),
+    url(r'^connected/?$', views.connected, name='connected'),
+    url(r'^qbo_request/?$', views.qbo_request, name='qbo_request'),
+    url(r'^revoke/?$', views.revoke, name='revoke'),
+    url(r'^refresh/?$', views.refresh, name='refresh'),
+    url(r'^user_info/?$', views.user_info, name='user_info'),
+    url(r'^migration/?$', views.migration, name='migration'),
 ]
--- quickbooks/views.py
+++ quickbooks/views.py
@@ -1,37 +1,199 @@
-from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.template import RequestContext
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.utils.translation import gettext as _
-from django.utils import timezone
-from django.urls import reverse_lazy
-from django.conf import settings
+from __future__ import absolute_import
 
-from django_messages.models import Message
-from django_messages.urls import *
-from django_messages.forms import ComposeForm
-from django_messages.utils import format_quote, get_user_model, get_username_field
+from requests import HTTPError
+import json
 
 from intuitlib.client import AuthClient
-from quickbooks import QuickBooks
-from quickbooks.objects.customer import Customer
+from intuitlib.migration import migrate
+from intuitlib.enums import Scopes
+from intuitlib.exceptions import AuthClientError
 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+from django.conf import settings
+from django.core import serializers
 
-User = get_user_model()
+from app.services import qbo_api_call
 
-@login_required
-def connect(request, template_name='quickbooks/connect.html'):
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def oauth(request):
     auth_client = AuthClient(
-            client_id='QBO_CLIENT_ID',
-            client_secret='QBO_CLIENT_SECRET',
-            environment='sandbox',
-            redirect_uri='QBO_REDIRECT_URI',
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT,
+    )
+
+    url = auth_client.get_authorization_url([Scopes.ACCOUNTING])
+    request.session['state'] = auth_client.state_token
+    return redirect(url)
+
+def openid(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT,
+    )
+
+    url = auth_client.get_authorization_url([Scopes.OPENID, Scopes.EMAIL])
+    request.session['state'] = auth_client.state_token
+    return redirect(url)
+
+def callback(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT, 
+        state_token=request.session.get('state', None),
+    )
+
+    state_tok = request.GET.get('state', None)
+    error = request.GET.get('error', None)
+    
+    if error == 'access_denied':
+        return redirect('app:index')
+    
+    if state_tok is None:
+        return HttpResponseBadRequest()
+    elif state_tok != auth_client.state_token:  
+        return HttpResponse('unauthorized', status=401)
+    
+    auth_code = request.GET.get('code', None)
+    realm_id = request.GET.get('realmId', None)
+    request.session['realm_id'] = realm_id
+
+    if auth_code is None:
+        return HttpResponseBadRequest()
+
+    try:
+        auth_client.get_bearer_token(auth_code, realm_id=realm_id)
+        request.session['access_token'] = auth_client.access_token
+        request.session['refresh_token'] = auth_client.refresh_token
+        request.session['id_token'] = auth_client.id_token
+    except AuthClientError as e:
+        # just printing status_code here but it can be used for retry workflows, etc
+        print(e.status_code)
+        print(e.content)
+        print(e.intuit_tid)
+    except Exception as e:
+        print(e)
+    return redirect('app:connected')
+
+def connected(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT, 
+        access_token=request.session.get('access_token', None), 
+        refresh_token=request.session.get('refresh_token', None), 
+        id_token=request.session.get('id_token', None),
+    )
+
+    if auth_client.id_token is not None:
+        return render(request, 'connected.html', context={'openid': True})
+    else:
+        return render(request, 'connected.html', context={'openid': False})
+
+def qbo_request(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT, 
+        access_token=request.session.get('access_token', None), 
+        refresh_token=request.session.get('refresh_token', None), 
+        realm_id=request.session.get('realm_id', None),
+    )
+
+    if auth_client.access_token is not None:
+        access_token = auth_client.access_token
+
+    if auth_client.realm_id is None:
+        raise ValueError('Realm id not specified.')
+    response = qbo_api_call(auth_client.access_token, auth_client.realm_id)
+    
+    if not response.ok:
+        return HttpResponse(' '.join([response.content, str(response.status_code)]))
+    else:
+        return HttpResponse(response.content)
+
+def user_info(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT, 
+        access_token=request.session.get('access_token', None), 
+        refresh_token=request.session.get('refresh_token', None), 
+        id_token=request.session.get('id_token', None),
+    )
+
+    try:
+        response = auth_client.get_user_info()
+    except ValueError:
+        return HttpResponse('id_token or access_token not found.')
+    except AuthClientError as e:
+        print(e.status_code)
+        print(e.intuit_tid)
+    return HttpResponse(response.content)
+        
+def refresh(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT, 
+        access_token=request.session.get('access_token', None), 
+        refresh_token=request.session.get('refresh_token', None), 
+    )
+
+    try:
+        auth_client.refresh() 
+    except AuthClientError as e:
+        print(e.status_code)
+        print(e.intuit_tid)
+    return HttpResponse('New refresh_token: {0}'.format(auth_client.refresh_token))
+
+def revoke(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT, 
+        access_token=request.session.get('access_token', None), 
+        refresh_token=request.session.get('refresh_token', None), 
+    )
+    try:
+        is_revoked = auth_client.revoke()
+    except AuthClientError as e:
+        print(e.status_code)
+        print(e.intuit_tid)
+    return HttpResponse('Revoke successful')
+
+
+def migration(request):
+    auth_client = AuthClient(
+        settings.CLIENT_ID, 
+        settings.CLIENT_SECRET, 
+        settings.REDIRECT_URI, 
+        settings.ENVIRONMENT,
+    )
+    try:
+        migrate(
+            settings.CONSUMER_KEY, 
+            settings.CONSUMER_SECRET, 
+            settings.ACCESS_KEY, 
+            settings.ACCESS_SECRET, 
+            auth_client, 
+            [Scopes.ACCOUNTING]
         )
-    client = QuickBooks(
-            auth_client=auth_client,
-            refresh_token='REFRESH_TOKEN',
-            company_id='COMPANY_ID',
-        )
-    customers = Customer.all(qb=client)
-    return render(request, template_name)
+    except AuthClientError as e:
+        print(e.status_code)
+        print(e.intuit_tid)
+    return HttpResponse('OAuth2 refresh_token {0}'.format(auth_client.refresh_token))
