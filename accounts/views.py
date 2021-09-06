885cb87911f1cf5551ea4050facbac1a1068f7e1
Triantafullenia-Doumani <Triantafulleniad@gmail.com>
2021-09-06 08:49:00 +0200

    fix webid

--- accounts/views.py
+++ accounts/views.py
@@ -162,7 +162,7 @@ class SignUpView(GuestOnlyView, FormView):
                 peppolID = get_random_string(20)
                 act.peppolID = peppolID
 
-            webID = 'https://demo-pondersource-net.herokuapp.com/'+user.username
+            webID = 'https://demo-pondersource-net/'+user.username
             act.webID = webID
             act.save()
 
